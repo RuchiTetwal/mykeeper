f5365bce28de2f61215e09a8e45794e1505ce1b8
Ruchi Tetwal <ruchi2452002@gmail.com>
2021-09-20 10:34:32 +0530

    first commit

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+
+function Footer(){
+    const year = new Date().getFullYear();
+
+    return <footer>
+        <p>Copyright ⓒ {year}</p>
+    </footer>
+}
+
+export default Footer;
